4e4b040a440df11c12212e12c5c0ebf5cbb385f4
Doug Chapman <dougch@amazon.com>
2021-05-17 16:04:32 -0700

    Shorten bench names

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,11 +3,11 @@ use criterion_gettingstarted::{fibonacci_slow,fibonacci_fast};
 
 
 pub fn criterion_bench_fib1(c: &mut Criterion) {
-    c.bench_function("fib 20 - slow", |b| b.iter(|| fibonacci_slow(black_box(20))));
+    c.bench_function("fib20-slow", |b| b.iter(|| fibonacci_slow(black_box(20))));
 }
 
 pub fn criterion_bench_fib2(c: &mut Criterion) {
-    c.bench_function("fib 20 - fast", |b| b.iter(|| fibonacci_fast(black_box(20))));
+    c.bench_function("fib20-fast", |b| b.iter(|| fibonacci_fast(black_box(20))));
 }
 
 criterion_group!(benches, criterion_bench_fib1, criterion_bench_fib2 );
